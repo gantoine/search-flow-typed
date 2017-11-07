a298f0b0ceb83ee00decaec673c2c023b2ca7920
Georges-Antoine Assi <georges.antoine.assi@gmail.com>
2017-11-06 20:48:04 -0500

    Switched to Algolia for searching

--- flow-typed/npm/underscore_v1.x.x.js
+++ flow-typed/npm/underscore_v1.x.x.js
@@ -1,5 +1,5 @@
-// flow-typed signature: 305cde02db6c44d9fde326af152c452c
-// flow-typed version: b841660c6f/underscore_v1.x.x/flow_>=v0.38.x
+// flow-typed signature: d79d7a391b2d6131fadd6c8aea82781a
+// flow-typed version: d6519a5bfc/underscore_v1.x.x/flow_>=v0.50.x
 
 // @flow
 /* eslint-disable */
@@ -287,10 +287,12 @@ declare module "underscore" {
     find<T>(list: T[], predicate: (val: T)=>boolean): ?T;
     detect<T>(list: T[], predicate: (val: T)=>boolean): ?T;
 
-    filter<T>(o: {[key:string]: T}, pred: (val: T, k: string)=>boolean): T[];
-    filter<T>(a: T[], pred: (val: T, k: string)=>boolean): T[];
-    select<T>(o: {[key:string]: T}, pred: (val: T, k: string)=>boolean): T[];
-    select<T>(a: T[], pred: (val: T, k: string)=>boolean): T[];
+    filter<T>(o: {[key:string]: T}, pred: (val: T, k: string, o: {[string]: T})=>boolean): T[];
+    filter<T>(a: T[], pred: (val: T, k: number, a: T[])=>boolean): T[];
+    filter<T>(o: {[string]: T}[], pred: {[string]: T}): T[];
+    select<T>(o: {[key:string]: T}, pred: (val: T, k: string, o: {[string]: T})=>boolean): T[];
+    select<T>(a: T[], pred: (val: T, k: number, a: T[])=>boolean): T[];
+    select<T>(o: {[string]: T}[], pred: {[string]: T}): T[];
     where<T>(list: Array<T>, properties: Object): Array<T>;
     findWhere<T>(list: Array<T>, properties: {[key:string]: any}): ?T;
 
@@ -447,12 +449,12 @@ declare module "underscore" {
     invert<K, V>(object: {[keys: K]: V}): {[keys: V]: K};
     // TODO: _.create
     functions(object: Object): Array<string>;
-    findKey(object: Object, predicate: () => boolean, context?: mixed): ?string;
+    findKey<K, V>(object: {[keys: K]: V}, predicate: (V, K, {[keys: K]: V}) => boolean, context?: mixed): ?string;
     extend: typeof $underscore$Extend;
     extendOwn: typeof $underscore$Extend;
-    pick<K, V>(object: {[keys: K]: V}): {[keys: K]: V};
-    omit<K, V>(object: {[keys: K]: V}): {[keys: K]: V};
-    defaults<K, V>(defaults: {[keys: K]: V}, more: {[keys: K]: V}): {[keys: K]: V};
+    pick<K, V>(object: {[keys: K]: V}, predicate?: K): {[keys: K]: V};
+    omit<K, V>(object: {[keys: K]: V}, predicate?: K): {[keys: K]: V};
+    defaults(defaults: ?Object, ...mores?: Array<Object>): Object;
     clone<O: {}>(object: O): O;
     tap<O>(object: O): O;
     has(object: Object, key: string): boolean;
@@ -495,7 +497,7 @@ declare module "underscore" {
     invert(): UnderscoreChainedObject<WrappedObj>;
     // TODO: _.create
     functions(): UnderscoreChainedList<string>;
-    findKey(predicate: () => boolean, context?: mixed): UnderscoreChainedValue<?string>;
+    findKey(predicate: ($Values<WrappedObj>, $Keys<WrappedObj>, WrappedObj) => boolean, context?: mixed): UnderscoreChainedValue<?$Keys<WrappedObj>>;
     // TODO: Reimplement these when you can get them to return UnderscoreChainedObject
     // extend: ExtendParameterized<{[key: K]: V}>;
     // extendOwn: ExtendParameterized<{[key: K]: V}>>;
@@ -546,7 +548,7 @@ declare module "underscore" {
     // TODO: _.create
     functions(): Array<string>;
     find(predicate: (v: any, k: $Keys<WrappedObj>, obj: WrappedObj) => boolean): ?any;
-    findKey(predicate: () => boolean, context?: mixed): ?string;
+    findKey(predicate: ($Values<WrappedObj>, $Keys<WrappedObj>, WrappedObj) => boolean, context?: mixed): ?$Keys<WrappedObj>;
     extend: typeof $underscore$ExtendParameterized;
     extendOwn: typeof $underscore$ExtendParameterized;
     // TODO make these actually remove properties
@@ -613,6 +615,7 @@ declare module "underscore" {
     reduceRight<U>(iteratee: (memo: U, value: T, index?: number) => U, init: U): U;
     find(predicate: (value: T) => boolean): ?T;
     filter(predicate: (value: T) => boolean): Array<T>;
+    filter(predicate: {[string]: T}): Array<T>;    
     where(properties: Object): Array<T>;
     findWhere(properties: $Shape<T>): ?T;
     reject(predicate: (value: T) => boolean, context?: mixed): Array<T>;
--- src/utils/api.js
+++ src/utils/api.js
@@ -2,7 +2,12 @@
 
 import axios from 'axios'
 import _ from 'underscore'
+import algoliasearch from 'algoliasearch';
+
 import type {Path, Result, NetResult, ValidResult} from 'search-flow-typed'
+
+const client = algoliasearch("IJU4P8BKDG", "4ea5b10fc24b8e8c9ef8db338e25653a");
+const index = client.initIndex('flowtyped_definitions');
 
 function parsePath(path: Path): Result {
   const flow: any = /\/(flow_.+)/.exec(path.path);
@@ -37,10 +42,28 @@ function navigateDefinition(directory): NetResult[] {
     .value();
 };
 
+const algoliaSearch = _.debounce((params, cb) => {
+  index.search(params, (err, content) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+
+    cb(content.hits);
+  })
+}, 250);
+
 module.exports = {
   fetchDefinitions: () => {
     const endpoint: string = 'https://api.github.com/repos/flowtype/flow-typed/git/trees/master:definitions/npm?recursive=1';
     return axios.get(endpoint)
       .then(navigateDefinition);
+  },
+  search: (query, cb) => {
+    let params = {
+      query: query,
+      hitsPerPage: 20,
+    };
+    algoliaSearch(params, cb);
   }
 };
