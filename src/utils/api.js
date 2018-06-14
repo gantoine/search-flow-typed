8859b53de5af240b70d0e2b73923a1275b53e756
Georges-Antoine <gassi@protonmail.com>
2018-06-14 14:38:55 -0400

    Remove axios dependency

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,10 +1,6 @@
 // @flow
 
-import axios from 'axios'
-import _ from 'underscore'
 import algoliasearch from 'algoliasearch';
-
-import type {Path, Result, NetResult, ValidResult} from 'search-flow-typed'
 
 const client = algoliasearch("IJU4P8BKDG", "4ea5b10fc24b8e8c9ef8db338e25653a");
 const index = client.initIndex('flowtyped_definitions');
